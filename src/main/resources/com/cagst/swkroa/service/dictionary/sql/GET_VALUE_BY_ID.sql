b33a91fcebdcf24f0aff6aa58410ab15faebc793
craig-gaskill <cagstsolutions@gmail.com>
2019-01-25 06:23:34 -0600

    Fixed issues retrieving Dictionary Values.

--- src/main/resources/com/cagst/swkroa/service/dictionary/sql/GET_VALUE_BY_ID.sql
+++ src/main/resources/com/cagst/swkroa/service/dictionary/sql/GET_VALUE_BY_ID.sql
@@ -3,7 +3,7 @@ SELECT cv.codevalue_id
       ,cv.codevalue_display
       ,cv.codevalue_meaning
       ,cv.active_ind
-      ,cv.updt_cnt AS codevalue_updt_cnt
+      ,cv.updt_cnt
   FROM codeset cs
  INNER JOIN codevalue cv ON (cv.codeset_id = cs.codeset_id)
  WHERE cs.codeset_meaning = :dictionaryMeaning
